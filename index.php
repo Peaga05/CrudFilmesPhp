fae3d89acd39c345ef46f95b60c2e1b1720304c0
Paulo Henrique Serafim <phls321@hotmail.com>
2023-10-23 21:53:15 -0300

    Script do banco e esqueleto base

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+    include "./Assents/header.php"
+?>
+<button class="btn btn-primary">Entrar</button>
+<i class="bi bi-0-circle"></i>
+<?php
+    include "./Assents/footer.php"
+?>
